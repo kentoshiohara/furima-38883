d2c2aa4c04d61832c61de73a3de820632f87bc8b
kentoshiohara <120357291+kentoshiohara@users.noreply.github.com>
2023-01-24 23:25:49 +0900

    ログインと新規登録のバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,18 +2,19 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable
 
   PASSWORD_REGIX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
 
-  validates :email, presence: true
-  validates :password, presence: true, length: { minimum:6 }
-  validates :password, presence: true, format: { with: PASSWORD_REGIX }
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, confirmation: true,
+  length: { minimum:6, allow_blank: true}, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "is invalid. Include both letters and numbers" }
+  validates :password_confirmation, presence: true
   validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :family_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+  validates :family_name_kana, presence: true, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
   validates :date_of_birth, presence: true
 
 end
