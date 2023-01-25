4a363a23587886998c36759c90069475acfd54b9
kentoshiohara <120357291+kentoshiohara@users.noreply.github.com>
2023-01-25 19:27:56 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,25 @@
 class ItemsController < ApplicationController
+
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    item = Item.create(items_params)
+    if item.save
+      redirect_to :index
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def items_params
+    params.require(:item).permit(:name, :description, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :days_id, :price, :image).merge(user_id: current_user.id) 
+  end
+  
 end
