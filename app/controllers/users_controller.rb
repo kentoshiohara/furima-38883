7be8c89456f3e540154ffbc0e269911646e219db
kentoshiohara <120357291+kentoshiohara@users.noreply.github.com>
2023-01-25 11:03:13 +0900

    ユーザーコントローラー作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.create(user_params)
+    if user.save?
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+end
