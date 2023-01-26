2a93086cf56af7e35e07eb655f189c0f260b8767
kentoshiohara <120357291+kentoshiohara@users.noreply.github.com>
2023-01-26 15:15:28 +0900

    Itemモデルの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,7 +13,7 @@ class Item < ApplicationRecord
     validates :name
     validates :description
     validates :price,
-              numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is invalid' }, format: { with: /\A[0-9]+\z/i }
+              numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is invalid' }
     validates :image
   end
 
