29909160a6f213c98737b2b55fb2835d01fa78b4
kentoshiohara <120357291+kentoshiohara@users.noreply.github.com>
2023-01-26 14:22:41 +0900

    モデルの単体テストコードの作成

---
+++ spec/factories/items.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :item do
+    name                  {Faker::Book.title}
+    description           {Faker::Lorem.sentence}
+    price                 {500}
+    category_id           {2}
+    status_id             {2}
+    delivery_fee_id       {2}
+    prefecture_id         {2}
+    days_id               {2}
+    association :user
+
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end 
+  end
+end
